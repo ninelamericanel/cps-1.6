e5b2764e8e3b09bb0af6edf47e9f0637329ed3c5
ninelamericanel <ninelamericanel@gmail.com>
2022-02-07 10:44:05 +0300

    create viewSwiper function for swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,23 +1,27 @@
 import Swiper, {Pagination} from "swiper";
 
+import {screenWidth} from "./variable";
+
 import 'swiper/swiper.scss';
 import 'swiper/modules/pagination/pagination.scss';
 
 Swiper.use([Pagination]);
 
+function viewSwiper () {
+  return screenWidth < 768 ? new Swiper(".swiper-container", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    slidesPerView: 'auto',
+    slidesPerColumn: 1,
+  }) : console.log('!!!');
+}
 
-window.addEventListener('resize', () => {
+viewSwiper();
 
-  return window.innerWidth < 768 ? new Swiper(".swiper-container", {
-      pagination: {
-        el: ".swiper-pagination",
-        type: "bullets",
-        clickable: true,
-      },
-      slidesPerView: 'auto',
-      slidesPerColumn: 1,
-    }) : null;
-})
+window.addEventListener('resize', viewSwiper);
 
 
 
