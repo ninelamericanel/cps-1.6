817f7e9cfe5d5fffef5163938640fef230bceaf3
ninelamericanel <ninelamericanel@gmail.com>
2022-02-08 19:07:30 +0300

    added stop scroll when popup open

--- src/js/popup-btn.js
+++ src/js/popup-btn.js
@@ -2,29 +2,39 @@ import {screenWidth} from "./variable";
 
 const openButtons = document.querySelectorAll('.open-btn');
 const closeButtons = document.querySelectorAll('.popup__btn-close');
-const popupOutside  = document.querySelectorAll('.popup__outside');
+const popupOutside = document.querySelectorAll('.popup__outside');
 
 const menuPopup = document.getElementById('menu');
 const feedbackPopup = document.getElementById('feedback');
 const callPopup = document.getElementById('call');
 
 popupOutside.forEach(el => {
-  el.addEventListener('click', () => listenerPopup(el, 'add'))
+  el.addEventListener('click', () => {
+    document.body.style.cssText = 'overflow-y: scroll';
+    listenerPopup(el, 'add')
+  })
 })
 
 openButtons.forEach(btn => {
-  btn.addEventListener('click', () => listenerPopup(btn, 'remove'))
+  btn.addEventListener('click', () => {
+    document.body.style.cssText = 'overflow-y: hidden';
+    listenerPopup(btn, 'remove')
+  })
 });
 
 closeButtons.forEach(btn => {
-  btn.addEventListener('click', () => listenerPopup(btn, 'add'))
+  btn.addEventListener('click', () => {
+    document.body.style.cssText = 'overflow-y: scroll';
+    listenerPopup(btn, 'add')
+  })
 })
 
 const changerClass = (name, action) => {
   name.classList[action]('popup--close');
 }
 
-function listenerPopup (btn, action) {
+function listenerPopup(btn, action) {
+
 
   if (btn.dataset.name === 'menu' && screenWidth < 1366) {
     changerClass(menuPopup, action);
