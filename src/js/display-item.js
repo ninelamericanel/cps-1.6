3c666b8c29d17c7ac2904d1c14259ccdfceeee7a
ninelamericanel <ninelamericanel@gmail.com>
2022-02-08 13:37:11 +0300

    rename display file, and refactored code

---
+++ src/js/display-item.js
@@ -0,0 +1,36 @@
+import {screenWidth} from "./variable";
+
+const listBrand = document.querySelectorAll('.repair__item.brand');
+const listEquipment = document.querySelectorAll('.repair__item.equipment')
+
+const toDisplay = () => {
+
+  if (screenWidth >= 768 && screenWidth < 1366) {
+
+    listBrand.forEach((el, index) => {
+      if (index >= 6) {
+        el.classList.add('repair__item--hide')
+      }
+    })
+
+    listEquipment.forEach((el, index) => {
+      if (index >= 3) {
+        el.classList.add('repair__item--hide')
+      }
+    })
+  } else if (screenWidth >= 1366) {
+    listBrand.forEach((el, index) => {
+      if (index >= 8) {
+        el.classList.add('repair__item--hide')
+      }
+    })
+    listEquipment.forEach((el, index) => {
+      if (index >= 4) {
+        el.classList.add('repair__item--hide')
+      }
+    })
+  }
+
+}
+
+toDisplay();
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
+import './display-item';
 import '../scss/style.scss';
+
 import './swiper';
-import './display';
+
+
 import './menu';
 import './popup-btn';
 
-
 import './display-all';
 import './read-more';
-import './menu';
 
