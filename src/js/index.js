dde65290e3d3cd4646299727309966c0c4f879e4
ninelamericanel <ninelamericanel@gmail.com>
2022-02-09 16:43:02 +0300

    fix animation for popup

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 import './display-item';
-import './popup-left-display';
+import './popup-left-display2';
 import '../scss/style.scss';
 
 import './swiper';
 
 
 import './menu';
-import './popup-btn';
+import './popup-btn2';
 
 import './btn-item';
 import './read-more-btn';
