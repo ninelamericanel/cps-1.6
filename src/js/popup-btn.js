69b6107de6537b616ced771cc4cb35f6a5f7f749
ninelamericanel <ninelamericanel@gmail.com>
2022-02-09 13:00:47 +0300

    fix popup

--- src/js/popup-btn.js
+++ src/js/popup-btn.js
@@ -44,3 +44,51 @@ function listenerPopup(btn, action) {
     changerClass(callPopup, action);
   }
 };
+
+// import {screenWidth} from "./variable";
+//
+// const openButtons = document.querySelectorAll('.open-btn');
+// const closeButtons = document.querySelectorAll('.popup__btn-close');
+// const popupOutside = document.querySelectorAll('.popup__outside');
+//
+// const menuPopup = document.getElementById('menu');
+// const feedbackPopup = document.getElementById('feedback');
+// const callPopup = document.getElementById('call');
+//
+// popupOutside.forEach(el => {
+//   el.addEventListener('click', () => {
+//     document.body.style.cssText = 'overflow-y: scroll';
+//     listenerPopup(el, 'remove')
+//   })
+// })
+//
+// openButtons.forEach(btn => {
+//   btn.addEventListener('click', () => {
+//     document.body.style.cssText = 'overflow-y: hidden';
+//     listenerPopup(btn, 'add')
+//   })
+// });
+//
+// closeButtons.forEach(btn => {
+//   btn.addEventListener('click', () => {
+//     document.body.style.cssText = 'overflow-y: scroll';
+//     listenerPopup(btn, 'remove')
+//   })
+// })
+//
+// const changerClass = (name, action) => {
+//   name.classList[action]('popup--open');
+// }
+//
+// function listenerPopup(btn, action) {
+//
+//
+//   if (btn.dataset.name === 'menu' && screenWidth < 1366) {
+//     changerClass(menuPopup, action);
+//   } else if (btn.dataset.name === 'feedback') {
+//     changerClass(feedbackPopup, action);
+//   } else if (btn.dataset.name === 'call') {
+//     changerClass(callPopup, action);
+//   }
+// };
+//
--- src/js/popup-left-display.js
+++ src/js/popup-left-display.js
@@ -13,12 +13,13 @@ function display () {
 
 display();
 
+//
 // import {screenWidth} from "./variable";
 //
 // const popup = document.querySelector('.popup--left');
 //
 // function display () {
-//   // console.log(screenWidth > 1366)
+//
 //   if (screenWidth >= 1366) {
 //     popup.classList.add('popup--open');
 //   } else {
